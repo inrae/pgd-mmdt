046dc074923554e6c20d0699d8d4b0e47e45a49c
djacob65@gmail.com <djacob65@gmail.com>
2024-08-12 16:49:23 +0200

    2.1

--- web/js/autocomplete/bioportal_typeahead.js
+++ web/js/autocomplete/bioportal_typeahead.js
@@ -18,53 +18,53 @@ var bioportal_typeahead = function (idName, ontology)
 		limitview : bioportal_limit,
 		async: true,
 		source: function (query, processSync, processAsync) {
-				url = bioportal_url+bioportal_options+ontology.replace(/:/g,',')+'&q=*'+encodeURIComponent(query)+'*&response=json&callback=?'
-				if (DEBUG) console.log('GET '+url)
-				return $.ajax({
-						url: url,
-						type: 'GET',
-						dataType: 'json',
-						success: function (json) {
-								res=[];
-								arr = json.data.split(/~.~/);
-								arr.forEach((el) => res.push(JSON.parse('{"label":"'+el.split('|')[0]+'", "ontology":"'+el.split('|')[3]+'"}' )));
-								return processAsync(res);
-						}
-				});
+			url = bioportal_url+bioportal_options+ontology.replace(/:/g,',')+'&q=*'+encodeURIComponent(query)+'*&response=json&callback=?'
+			if (DEBUG) console.log('GET '+url)
+			return $.ajax({
+				url: url,
+				type: 'GET',
+				dataType: 'json',
+				success: function (json) {
+					res=[];
+					arr = json.data.split(/~.~/);
+					arr.forEach((el) => res.push(JSON.parse('{"label":"'+el.split('|')[0]+'", "ontology":"'+el.split('|')[3]+'"}' )));
+					return processAsync(res);
+				}
+			});
 		},
 		templates: {
-				header: [
-						'<div class="empty-message" style="display: inline-block;">',
-						'<img src="'+bioportal_logo+'" style="width:30px;"/>&nbsp;&nbsp;'+
-						'<b><i><font color="grey" size=-1>&nbsp;'+shrink_ontolist(ontology,40)+'</font></i></b>',
-						'</div>'
-				].join('\n'),
-				pending: [
-						'<div class="empty-message" style="display: inline-block;">',
-						'<img src="'+bioportal_logo+'" style="width:30px;"/>&nbsp;&nbsp;'+
-						'<b><i>&nbsp;... wait ...</i></b>',
-						'</div>'
-				].join('\n'),
-				empty: [
-						'<div class="empty-message" style="display: inline-block;">',
-						'<img src="'+bioportal_logo+'" style="width:30px;"/>&nbsp;&nbsp;'+
-						'<b><i>&nbsp; this term seems not exist !! </i></b>',
-						'</div>'
-				].join('\n'),
-				footer: [
-						'<div class="empty-message" style="display: inline-block;">',
-						'<b><i><font color="grey" size=-1>Result provided by BioPortal NCBO</font></i></b>',
-						'</div>'
-				].join('\n'),
-				suggestion: function (data) {
-						return '<p>'+data.label+'&nbsp;<font color=grey><i>('+data.ontology+')</i></font></p>';
-				}
+			header: [
+				'<div class="empty-message" style="display: inline-block;">',
+				'<img src="'+bioportal_logo+'" style="width:30px;"/>&nbsp;&nbsp;'+
+				'<b><i><font color="grey" size=-1>&nbsp;'+shrink_ontolist(ontology,40)+'</font></i></b>',
+				'</div>'
+			].join('\n'),
+			pending: [
+				'<div class="empty-message" style="display: inline-block;">',
+				'<img src="'+bioportal_logo+'" style="width:30px;"/>&nbsp;&nbsp;'+
+				'<b><i>&nbsp;... wait ...</i></b>',
+				'</div>'
+			].join('\n'),
+			empty: [
+				'<div class="empty-message" style="display: inline-block;">',
+				'<img src="'+bioportal_logo+'" style="width:30px;"/>&nbsp;&nbsp;'+
+				'<b><i>&nbsp; this term seems not exist !! </i></b>',
+				'</div>'
+			].join('\n'),
+			footer: [
+				'<div class="empty-message" style="display: inline-block;">',
+				'<b><i><font color="grey" size=-1>Result provided by BioPortal NCBO</font></i></b>',
+				'</div>'
+			].join('\n'),
+			suggestion: function (data) {
+				return '<p>'+data.label+'&nbsp;<font color=grey><i>('+data.ontology+')</i></font></p>';
+			}
 		},
 		display: function (data) {
-				return data.label;
+			return data.label;
 		},
 		value: function(data) {
-				return data.label;
+			return data.label;
 		}
 	});
 }
